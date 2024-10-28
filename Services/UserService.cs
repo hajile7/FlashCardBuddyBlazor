5e964f7a6da31105b5cd28197b574b8043a67dbc
Eli <elireid.28@gmail.com>
2024-10-28 15:44:58 -0400

    Got login working and spruced up registration

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 using FlashCardBuddyBlazor.Models;
 
@@ -24,14 +25,30 @@ namespace FlashCardBuddyBlazor.Services
 
         public async Task<bool> PostUserAsync(PostUserDTO user) 
         {
-          var response = await _httpclient.PostAsJsonAsync($"{url}/api/User",user);
-           return response.IsSuccessStatusCode;
+            var response = await _httpclient.PostAsJsonAsync($"{url}/api/User",user);
+            return response.IsSuccessStatusCode;
         }
 
         public async Task<UserDTO> LoginAsync(string username, string password)
         {
-            var result = await _httpclient.GetFromJsonAsync<UserDTO>($"{url}/api/User/Login?username={username}&password={password}");
-            return result;
+            var response = await _httpclient.GetAsync($"{url}/api/User/Login?username={username}&password={password}");
+            if(response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<UserDTO>();
+                return result ?? throw new Exception("An unknown error has occured");
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                throw new Exception("User not found");
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                throw new Exception("Invalid login credentials");
+            }
+            else
+            {
+                throw new Exception("An unknown error has occured");
+            }
         }
 
     }
